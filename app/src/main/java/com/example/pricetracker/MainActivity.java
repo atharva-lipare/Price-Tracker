f556c3c420929f5ed0bfd0aad6be9f165ad7c605
Atharva Lipare <atharvaslipare@gmail.com>
2020-11-24 17:28:55 +0530

    :sparkles: Add bottom navbar

--- app/src/main/java/com/example/pricetracker/MainActivity.java
+++ app/src/main/java/com/example/pricetracker/MainActivity.java
@@ -1,71 +1,39 @@
 package com.example.pricetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.SearchView;
-import android.widget.TextView;
+import android.view.MenuItem;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    SearchView searchView;
-    Button scrapeButton;
-    TextView amznView, fkartView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        searchView = (SearchView)findViewById(R.id.searchView);
-        scrapeButton = (Button)findViewById(R.id.button);
-        amznView = (TextView)findViewById(R.id.textView2);
-        fkartView = (TextView)findViewById(R.id.textView3);
-        scrapeButton.setOnClickListener(new View.OnClickListener() {
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Log.d("success", searchView.getQuery().toString());
-                new doIt().execute();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+                return true;
             }
         });
-    }
-
-    public class doIt extends AsyncTask<Void, Void, Void> {
-        ArrayList<String> as1Names, as1Prices, fs1Names, fs1Prices;
-        AmazonScraper as1;
-        FlipkartScraper fs1;
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                as1 = new AmazonScraper(searchView.getQuery().toString());
-                fs1 = new FlipkartScraper(searchView.getQuery().toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            as1Names = as1.getNames();
-            as1Prices = as1.getPrices();
-            amznView.setText("Amazon\n");
-            for (int i = 0; i < Math.min(as1Names.size(), 3); i++) {
-                amznView.append(as1Names.get(i) + ": " + as1Prices.get(i) + "\n");
-            }
-            fs1Names = fs1.getNames();
-            fs1Prices = fs1.getPrices();
-            fkartView.setText("Flipkart\n");
-            for (int i = 0; i < Math.min(fs1Names.size(), 3); i++) {
-                fkartView.append(fs1Names.get(i) + ": " + fs1Prices.get(i) + "\n");
-            }
-        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
     }
 }
